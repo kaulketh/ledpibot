4f60dc2162226106314110dc9b8e1289ca35c7eb
Thomas Kaulke <tkaulke@bekast.de>
2020-03-13 10:22:20 +0100

    Update auto reboot (service.py)

--- control/service.py
+++ control/service.py
@@ -16,6 +16,7 @@ import subprocess
 import time
 
 import logger
+from config import rebooted
 from control import CountdownThread
 
 NAME = "Service"
@@ -111,32 +112,27 @@ def update_bot(log_msg: str):
 
 
 def init_auto_reboot():
-    name = "Auto reboot"
-
     def is_midnight():
         hour = datetime.datetime.now().hour
         minute = datetime.datetime.now().minute
         sec = datetime.datetime.now().second
-        about_midnight = ((hour == minute == 0) and (0 <= sec <= 5))
-        # for test only
-        # anytime = (hour == 21 and minute == 30 and sec == 0)
-        # return anytime
+        about_midnight = (hour == 0 and minute == 0 and sec <= 5)
         return about_midnight
 
     class AutoReboot(CountdownThread):
         def __init__(self):
-            super(AutoReboot, self).__init__(None, None, name=name, n=0)
+            super(AutoReboot, self).__init__(None, None, name="auto reboot", n=0)
 
         def _process(self):
             pass
 
         def run(self):
-            LOG.info("Auto reboot initialized for midnight")
+            LOG.info("Auto reboot initialized for midnight.")
             while not is_midnight():
                 time.sleep(2)
             from bot import external_request
-            external_request(name)
-            reboot_device(name)
+            external_request(rebooted)
+            reboot_device("auto reboot")
 
     AutoReboot().start()
     return
