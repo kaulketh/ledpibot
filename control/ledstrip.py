e2b0126a77571daa7c76b159ed712917d3131f0e
Thomas <kaulketh@gmail.com>
2020-03-23 01:13:20 +0100

    Update ledstrip.py

--- control/ledstrip.py
+++ control/ledstrip.py
@@ -21,29 +21,31 @@ class Strip(object):
     name = "LED Strip"
 
     def __init__(self, count, pin, hz, dma, invert, brightness):
-        self.count = count
-        self.pin = pin
-        self.hz = hz
-        self.dma = dma
-        self.invert = invert
-        self.brightness = brightness
-        self.logger.debug(f"Create {self}")
-        self.strip = Adafruit_NeoPixel(self.count, self.pin, self.hz, self.dma, self.invert, self.brightness)
-        self.logger.debug(f"Initialized: {self.strip}")
-        self.strip.begin()
+        self.__logger = Strip.logger
+        self.__count = count
+        self.__pin = pin
+        self.__hz = hz
+        self.__dma = dma
+        self.__invert = invert
+        self.__brightness = brightness
+        self.__logger.debug(f"Create {self}")
+        self.__strip = Adafruit_NeoPixel(self.__count, self.__pin, self.__hz, self.__dma, self.__invert,
+                                         self.__brightness)
+        self.__logger.debug(f"Initialized: {self.__strip}")
+        self.__strip.begin()
 
     def __repr__(self):
         return (
             f"{self.name}: "
-            f"COUNT:{self.count}, "
-            f"PIN:{self.pin}, "
-            f"FREQ:{self.hz}, "
-            f"DMA:{self.dma}, "
-            f"INVERT:{self.invert}, "
-            f"BRIGHTN.:{self.brightness}")
+            f"COUNT:{self.__count}, "
+            f"PIN:{self.__pin}, "
+            f"FREQ:{self.__hz}, "
+            f"DMA:{self.__dma}, "
+            f"INVERT:{self.__invert}, "
+            f"BRIGHTN.:{self.__brightness}")
 
-    def get_instance(self):
-        return self.strip
+    def get_strip(self):
+        return self.__strip
 
     @classmethod
     def setup(cls, s: Adafruit_NeoPixel):
@@ -66,7 +68,7 @@ def set_brightness_depending_on_daytime(s: Adafruit_NeoPixel):
     return Strip.setup(s)
 
 
-STRIP = Strip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS).get_instance()
+STRIP = Strip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS).get_strip()
 
 if __name__ == '__main__':
     pass
