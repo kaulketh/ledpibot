87b00870c68aa022360ecff1d694e163072ff45f
Thomas <kaulketh@gmail.com>
2020-04-09 11:31:44 +0200

    Update

--- bot.py
+++ bot.py
@@ -180,15 +180,19 @@ class LedPiBot:
                 self.__send(chat_id, m_rebooted, reply_markup=self.rm_kb)
                 service.reboot_device(m_rebooted)
             elif command == service.Service.c_info:
-                info = service.system_info()
-                self.__send(chat_id, info, reply_markup=self.rm_kb)
-                self.__log.info(info.replace("\n", "").replace(" ", ""))
+                if self.__stop_function(chat_id):
+                    info = service.system_info()
+                    self.__send(chat_id, info, reply_markup=self.rm_kb)
+                    self.__log.info(info.replace("\n", "").replace(" ", ""))
             elif command == service.Service.c_update:
-                self.__send(chat_id, m_updated, reply_markup=self.rm_kb)
-                update_bot(m_updated)
-            elif command == service.Service.c_help:
-                self.__send(chat_id, service.Service.help(),
-                            reply_markup=self.rm_kb)
+                if self.__stop_function(chat_id):
+                    self.__send(chat_id, m_updated, reply_markup=self.rm_kb)
+                    update_bot(m_updated)
+            elif command == service.Service.c_help \
+                    or command == service.Service.c_help.lower():
+                if self.__stop_function(chat_id):
+                    self.__send(chat_id, service.get_help_text(),
+                                reply_markup=self.rm_kb)
 
             # all other commands
             elif any(c for c in commands if (command == c)):
