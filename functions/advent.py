2e34420e7622e5b58ce65ab2d1013a5006e4957a
Thomas <kaulketh@gmail.com>
2022-01-04 12:34:14 +0100

    fix/update advent.py

--- functions/advent.py
+++ functions/advent.py
@@ -16,16 +16,18 @@ from functions.candles import BLUE, GREEN, RED, candle
 from functions.effects import clear, theater_chase
 from logger import LOGGER
 
-# advent color
+# colors
+# advent
 A_RED = 255
 A_GREEN = 30
 A_BLUE = 0
 
-YEAR = datetime.datetime.now().year
-XMAS = datetime.date(YEAR, 12, 25)
-FIRST_POSSIBLE_ADVENT = datetime.date(YEAR, 11, 27)
-NOW = datetime.datetime.now().date()
-IS_ADVENT_PERIOD = NOW >= FIRST_POSSIBLE_ADVENT
+# first LED
+ZERO = (210, 70, 0)
+
+
+def __is_advent_period(year):
+    return datetime.datetime.now().date() >= datetime.date(year, 11, 27)
 
 
 def __set_rand_brightness(led, red, green, blue, stripe):
@@ -43,8 +45,7 @@ def __allsundays(year):
     """
     # start with Nov-27 which is the first possible day of Advent
     # 4 advents == 4 weeks == 28 days -> 25th Dec - 28 day = 27th Nov ;-)
-
-    d = FIRST_POSSIBLE_ADVENT
+    d = datetime.date(year, 11, 27)
     # find the next Sunday after the above date
     d += datetime.timedelta(days=6 - d.weekday())
     while d.year == year:
@@ -54,11 +55,13 @@ def __allsundays(year):
 
 def __advent_cycle(stripe):
     advent = []
+
     try:
         # collect advent dates
-        for d in __allsundays(YEAR):
-            if d < XMAS:
-                advent.append(d.day)
+        year = datetime.datetime.now().year
+        for sunday in __allsundays(year):
+            if sunday < datetime.date(year, 12, 25):
+                advent.append(sunday.day)
 
         day = datetime.datetime.now().day
         # ensure only the day related LEDs are set as candle
@@ -67,8 +70,7 @@ def __advent_cycle(stripe):
                 # different color and brightness per day
                 # set up first LED as advent because it's before 1st December
                 if advent[0] in [27, 28, 29, 30]:
-                    __set_rand_brightness(0,
-                                          210, 70, 0, stripe)
+                    __set_rand_brightness(0, ZERO[0], ZERO[1], ZERO[2], stripe)
                 # set up other advents in december
                 if (i + 1) in advent:
                     __set_rand_brightness(i, A_RED, A_GREEN, A_BLUE, stripe)
@@ -93,8 +95,10 @@ def run_advent(stripe):
     LOGGER.debug("running...")
     from control import get_stop_flag
     i = 1
+
     while not get_stop_flag():
-        if IS_ADVENT_PERIOD:
+        year = datetime.datetime.now().year
+        if __is_advent_period(year):
             __advent_cycle(stripe)
         else:
             while i > 0:
@@ -102,7 +106,7 @@ def run_advent(stripe):
                     f"Wrong period to show xmas/advent animation, "
                     f"it\'s {time.strftime('%A, %d.%B %Y')}!")
                 i -= 1
-            theater_chase(stripe, Color(A_RED, A_GREEN, A_BLUE))
+            theater_chase(stripe, Color(ZERO[0], ZERO[1], ZERO[2]))
     clear(stripe)
 
 
