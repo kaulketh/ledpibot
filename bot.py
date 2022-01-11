76bc1973c4e212da4c9def80ee41ef04feb0c7f3
Thomas <kaulketh@gmail.com>
2022-01-11 11:35:16 +0100

    update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ __maintainer__ = "Thomas Kaulke"
 __status__ = "Production"
 __doc__ = "Call your preferred class here."
 
-from concurrent.futures import ThreadPoolExecutor
+import threading
 
 from urllib3.exceptions import HTTPError
 
@@ -22,11 +22,11 @@ def run():
 
 
 if __name__ == '__main__':
+
     try:
-        go_on = ThreadPoolExecutor()
-        go_on.submit(run)
-        go_on.submit(wink_w_eyes)
-        go_on.shutdown()
+        threading.Thread(target=wink_w_eyes).start()
+        run()
+
     except HTTPError as e:
         LOGGER.error("HTTP Error occurs!", e)
         exit()
