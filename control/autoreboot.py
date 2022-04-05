fc1e37e317c4b4ff135c267494bd22aeffa2a8d9
Thomas <kaulketh@gmail.com>
2022-04-05 20:00:44 +0200

    minor changes and test autoreboot

--- control/autoreboot.py
+++ control/autoreboot.py
@@ -20,7 +20,7 @@ class AutoReboot(LightFunction):
     def __init__(self, reboot_time: str, bot):
         self.__hour = reboot_time[:2]
         self.__minute = reboot_time[3:5]
-        self.__hm = int(self.__hour), int(self.__minute)
+        self.__hm = int(self.__hour), int(self.__minute) - 1
         self.__bot = bot
         self.__ONE_MINUTE = 60  # check interval
         super(AutoReboot, self).__init__(None, None, name=self.name,
@@ -43,22 +43,22 @@ class AutoReboot(LightFunction):
         try:
             self._logger.info(
                 f"{self.name} scheduled: "
-                f"{self.__hour}:{self.__minute} + 1")
+                f"{self.__hour}:{self.__minute}, "
+                f"seconds will be ignored")
             while not self.__time_to_reboot()[0]:
                 self._logger.debug(
                     f"reboot time not yet reached, "
                     f"recheck in {self.__ONE_MINUTE} seconds")
                 time.sleep(self.__ONE_MINUTE)
-            self._logger.debug(f"wait again {self.__ONE_MINUTE} seconds "
-                               f"to ensure next minute "
-                               f"after specified reboot time")
+            self._logger.debug(f"{self.__ONE_MINUTE} seconds "
+                               f"before specified reboot time")
             time.sleep(self.__ONE_MINUTE)
             self._logger.debug("force device reboot")
             self.__bot.external_request(msg=f"{self.name}\n{m_rebooted}",
                                         bot=self.__bot)
             reboot_device(self.name)
         except Exception as e:
-            # FIXME: throws ConnectionError... see log file
+            # FIXME: throws ConnectionResetError... see log file
             self._logger.error(f"{e}")
 
 
