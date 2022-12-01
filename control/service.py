011cc75f656aef7d1d3301ddb6b57429ada1e022
Thomas <kaulketh@gmail.com>
2022-12-01 09:58:14 +0100

    texts updated

--- control/service.py
+++ control/service.py
@@ -30,11 +30,11 @@ It is possible to call in-app commands at any time!
 *Call of any in-app command will break running function!*
 
 - *Functions/Animations:*
-    Clock 1:    One LED per 'pointer', red hours 'pointer', blue minutes 'pointer', warm yellow running seconds 'pointer' every 2.5 seconds.
-    Clock 2:    One LED per 'pointer', red hour, blue minute, lue running light every 2.5 minutes from the current 'minute' to the '12'.
-    Clock 3:    One LED per 'pointer', red hour, blue minute, green seconds 'scale' will extend every 2.5 seconds. There is also a 'dial', subtly warm yellow for each 'number'.
+    Clock 1:    One LED per 'watch hand', red hour hand, blue minute hand, warm yellow running second hand every 2.5 seconds.
+    Clock 2:    One LED per 'watch hand', red hour, blue minute, lue running light every 2.5 minutes from the current 'minute' to the '12'.
+    Clock 3:    One LED per 'watch hand', red hour, blue minute, green seconds 'scale' will extend every 2.5 seconds. There is also a 'dial', subtly warm yellow for each 'number'.
     Clock 4:    Colorful, full color 'scale' for minute, hour, second, 'scales' will be overridden and colors mixed/changed thus.
-    Clock 5:    Red hours 'pointer', blue minutes 'pointer' and warm yellow second 'pendulum' over all LEDs.
+    Clock 5:    Red hour hand, blue minute hand and warm yellow second 'pendulum' over all LEDs.
     Clock 6:    Similar Clock 4, but w/o seconds and Green and Blue as major colors.
     Advent:     Advent calendar, works in Advent time only! For every day of December will one LED flicker like a candlelight. If it is Advent Sunday it flickers red. Should be time before December but in Advent period all LEDs are working as candle light. If it is other than Advent time LEDs will circle in orange as warning!
     Candles:    Each LED simulates candlelight.
