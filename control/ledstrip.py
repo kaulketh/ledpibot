a342ab2bd240b726c431e01e55dbfc044cb93f85
Thomas <kaulketh@gmail.com>
2023-02-27 14:06:47 +0100

    OOP: minor changes ledstrip.py

--- control/ledstrip.py
+++ control/ledstrip.py
@@ -10,43 +10,15 @@ import datetime
 
 from rpi_ws281x import *
 
-from config import LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_BRIGHTNESS, \
-    LED_INVERT, LED_CUT_OFF_NIGHT, \
-    LED_CUT_OFF_MORNING, LED_BRIGHTNESS_DAY, \
-    LED_BRIGHTNESS_NIGHT
+from config import LED_BRIGHTNESS, LED_BRIGHTNESS_DAY, LED_BRIGHTNESS_NIGHT, \
+    LED_COUNT, LED_CUT_OFF_MORNING, LED_CUT_OFF_NIGHT, LED_DMA, LED_FREQ_HZ, \
+    LED_INVERT, LED_PIN
 from logger import LOGGER
 
 
 class Strip:
     name = "LED Strip"
-
-    def __init__(self, count, pin, hz, dma, invert, brightness):
-        self.__logger = LOGGER
-        self.__count = count
-        self.__pin = pin
-        self.__hz = hz
-        self.__dma = dma
-        self.__invert = invert
-        self.__brightness = brightness
-        self.__logger.debug(f"Create {self}")
-        self.__strip = Adafruit_NeoPixel(self.__count, self.__pin, self.__hz,
-                                         self.__dma, self.__invert,
-                                         self.__brightness)
-        self.__logger.debug(f"Initialized: {self.__strip}")
-        self.__strip.begin()
-
-    def __repr__(self):
-        return (
-            f"{self.name}: "
-            f"COUNT:{self.__count}, "
-            f"PIN:{self.__pin}, "
-            f"FREQ:{self.__hz}, "
-            f"DMA:{self.__dma}, "
-            f"INVERT:{self.__invert}, "
-            f"BRIGHTN.:{self.__brightness}")
-
-    def get_strip(self):
-        return self.__strip
+    log = LOGGER
 
     @classmethod
     def setup(cls, s: Adafruit_NeoPixel):
@@ -64,13 +36,41 @@ class Strip:
         b = s.getBrightness()
         return now, b
 
+    def __init__(self, count, pin, hz, dma, invert, brightness):
+        self.__count = count
+        self.__pin = pin
+        self.__hz = hz
+        self.__dma = dma
+        self.__invert = invert
+        self.__brightness = brightness
+        Strip.log.debug(f"Create {self}")
+        self.__strip = Adafruit_NeoPixel(self.__count, self.__pin, self.__hz,
+                                         self.__dma, self.__invert,
+                                         self.__brightness)
+        Strip.log.debug(f"Initialized: {self.__strip}")
+        self.__strip.begin()
+
+    def __repr__(self):
+        return (
+            f"{self.name}: "
+            f"COUNT:{self.__count}, "
+            f"PIN:{self.__pin}, "
+            f"FREQ:{self.__hz}, "
+            f"DMA:{self.__dma}, "
+            f"INVERT:{self.__invert}, "
+            f"BRIGHTN.:{self.__brightness}")
+
+    @property
+    def strip(self):
+        return self.__strip
+
 
 def set_brightness_depending_on_daytime(s: Adafruit_NeoPixel):
     return Strip.setup(s)
 
 
 STRIP = Strip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT,
-              LED_BRIGHTNESS).get_strip()
+              LED_BRIGHTNESS).strip
 
 if __name__ == '__main__':
     pass
