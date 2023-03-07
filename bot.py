931ee6f8f109e100b185f1dd68902e64a9672bbc
Thomas <44971681+kaulketh@users.noreply.github.com>
2023-03-07 15:29:10 +0100

    Update bot.py
    fix documentation and comment typos

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ __author__ = "Thomas Kaulke"
 __email__ = "kaulketh@gmail.com"
 __maintainer__ = "Thomas Kaulke"
 __status__ = "Production"
-__doc__ = "Call instance of bot bot_framework class."
+__doc__ = "Call instance of framework class."
 
 import threading
 
