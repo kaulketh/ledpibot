47c5d4c71d8f983a78c6f8368a6c7e294950e411
Thomas <kaulketh@gmail.com>
2024-01-04 18:15:36 +0100

    update logger

--- logger/__init__.py
+++ logger/__init__.py
@@ -2,10 +2,7 @@ from .logger import PreconfiguredLogger
 
 # instantiate logger
 LOGGER = PreconfiguredLogger.instance
-"""
-preconfigured Singleton logger instance, 
-for settings refer 'logger.yml'  
-"""
+"""preconfigured Singleton logger instance, for settings refer 'logger.yml'"""
 
 HISTORY = PreconfiguredLogger.HIS_LOG
 LOGS = PreconfiguredLogger.log_folder
